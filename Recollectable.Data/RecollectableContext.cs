94402507d8d55d7a37acff2a75023869c8339dee
Mike Joosten <mike.yaesu@gmail.com>
2018-07-18 23:09:57 +0200

    Added database context

---
+++ Recollectable.Data/RecollectableContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recollectable.Data
+{
+    public class RecollectableContext : DbContext
+    {
+        public RecollectableContext(DbContextOptions<RecollectableContext> options) 
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
